6cae6308e200ad16e1052045177a4a2369546675
Sergio Elías Pérez Camargo <ser.per.eli@gmail.com>
2024-06-08 11:16:09 -0500

    Editar selecciones

--- selecciones_editar.php
+++ selecciones_editar.php
@@ -24,6 +24,22 @@ $seleccion = $db->query($sql)->fetch_assoc();
                 required
             >
         </div>
+        <div class="mb-3">
+            <label for="grupo" class="form-label">Grupo</label>
+            <select name="grupo" id="grupo" class="form-control" required>
+                <option value="A">A</option>
+                <option value="B">B</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="url_bandera" class="form-label">Bandera</label>
+            <textarea name="url_bandera" id="url_bandera">
+                <?=  $seleccion['url_bandera'] ?>
+            </textarea>
+        </div>
+
         <input type="hidden" name="accion" value="1">
         <button type="submit" class="btn btn-primary">Guardar</button>
         <a href="usuarios.php" class="btn btn-secondary">Cancelar</a>
