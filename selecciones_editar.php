b4590a840810ff24bad7c2490776fd84f4acc81a
Sergio Elías Pérez Camargo <ser.per.eli@gmail.com>
2024-06-08 11:42:06 -0500

    actualización de formulario editar

--- selecciones_editar.php
+++ selecciones_editar.php
@@ -3,7 +3,23 @@ require_once('header.php');
 $id = $_GET['id'];
 $sql = "SELECT * FROM selecciones WHERE id = $id";
 $seleccion = $db->query($sql)->fetch_assoc();
-
+if (isset($_POST['accion'])){
+    $nombre = $_POST['nombre'];
+    $grupo = $_POST['grupo'];
+    $url_bandera = $_POST['url_bandera'];
+    $id = $_POST['id'];
+    
+    $sql = "INSERT INTO users (username,password) values ('$username',MD5('$password'));";
+    $result = $db->query($sql);
+    $db->close();
+    if($result) {
+        $_SESSION['flash_message'] = "Usuario creado";
+    }else{
+        $_SESSION['flash_message'] = "Error al crear usuario";
+    
+    }
+    header('LOCATION: usuarios.php');
+}
 ?>
 <div class="row">
     <div class="col-12">
@@ -27,10 +43,10 @@ $seleccion = $db->query($sql)->fetch_assoc();
         <div class="mb-3">
             <label for="grupo" class="form-label">Grupo</label>
             <select name="grupo" id="grupo" class="form-control" required>
-                <option value="A">A</option>
-                <option value="B">B</option>
-                <option value="C">C</option>
-                <option value="D">D</option>
+                <option value="A" <?php echo $seleccion['grupo'] == 'A'?'selected':''; ?>>A</option>
+                <option value="B" <?php echo $seleccion['grupo'] == 'B'?'selected':''; ?>>B</option>
+                <option value="C" <?= $seleccion['grupo'] == 'C'?'selected':''; ?>>C</option>
+                <option value="D" <?= $seleccion['grupo'] == 'D'?'selected':''; ?>>D</option>
             </select>
         </div>
         <div class="mb-3">
@@ -39,8 +55,9 @@ $seleccion = $db->query($sql)->fetch_assoc();
         </div>
 
         <input type="hidden" name="accion" value="1">
+        <input type="hidden" name="id" value="<?= $seleccion['id'] ?>">
         <button type="submit" class="btn btn-primary">Guardar</button>
-        <a href="usuarios.php" class="btn btn-secondary">Cancelar</a>
+        <a href="selecciones.php" class="btn btn-secondary">Cancelar</a>
     </form>
 </div>
 <?php
